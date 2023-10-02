5fa3b7ee11ab03ebb518df9be1846e15b7c04f61
nullshii <null>
2023-10-02 14:38:05 +0300

    Added game testing command

---
+++ app/Console/Commands/TestGameCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TelegramUser;
+use App\Services\GameService;
+use Illuminate\Console\Command;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class TestGameCommand extends Command
+{
+    protected $signature = "game:test";
+    protected $description = "Test game to log file.";
+
+    public function handle(GameService $game): void
+    {
+        $users = TelegramUser::where('is_subscribed', true)->get();
+
+        $export = $game->Export();
+
+        foreach ($users as $user) {
+            Telegram::sendMessage([
+                "chat_id" => $user->telegram_id,
+                "text" => $export
+            ]);
+        }
+    }
+}
