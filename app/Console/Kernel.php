494ef7e0776e3c29b02ae6f9b7109b74e6f25c19
nullshii <null>
2023-10-10 12:16:04 +0300

    uncommented back every minute schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-//        $schedule->command('game:send')->everyMinute();
+        $schedule->command('game:send')->everyMinute();
     }
 
     /**
